4983e43112cfa0fa5ace8255a16d92a49cd19ae9
nisan-cb <nnisan858@gmail.com>
2022-08-19 13:49:22 +0300

    import server files from prev project

---
+++ server/src/db.ts
@@ -0,0 +1,169 @@
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config()
+
+
+export default class DB {
+    client: Client;
+
+    constructor() {
+        this.client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        })
+
+    }
+
+
+    async createTables() {
+        await this.createBranchesTable();
+        await this.createClientsTable();
+        await this.createServicesTable();
+        await this.createRecordTable();
+    }
+
+    async connect() {
+        console.log('conecting...')
+        await this.client.connect();
+    }
+
+    // Create branches table
+    async createBranchesTable() {
+        await this.client.query(
+            `CREATE TABLE IF NOT EXISTS branches(
+                code SERIAL PRIMARY KEY,
+                city VARCHAR(10) NOT NULL
+            );`
+        );
+    }
+
+    // Create services table
+    async createServicesTable() {
+        await this.client.query(
+            `CREATE TABLE IF NOT EXISTS services(
+                code SERIAL PRIMARY KEY,
+                description VARCHAR(50) NOT NULL
+            );`
+        );
+    }
+
+    // Create clients table
+    async createClientsTable() {
+        await this.client.query(
+            `CREATE TABLE IF NOT EXISTS clients(
+                id INTEGER PRIMARY KEY,
+                name VARCHAR(10) NOT NULL,
+                phone_number VARCHAR(10) NOT NULL
+            );`
+        );
+    }
+
+    // Create records table
+    async createRecordTable() {
+        await this.client.query(
+            `CREATE TABLE IF NOT EXISTS records(
+                number SERIAL PRIMARY KEY,
+                branch_code INTEGER REFERENCES branches(code),
+                client_id INTEGER REFERENCES clients(id),
+                service_code INTEGER REFERENCES services(code)
+            );`
+        );
+    }
+
+    // insert new service
+    async insertNewService(description: string) {
+        await this.client.query(
+            'INSERT INTO services (description) VALUES ($1)',
+            [description]
+        );
+    }
+
+    // insert new client
+    async insertNewClient(id: number, name: string, phoneNumber: string) {
+        console.log(name);
+        await this.client.query(
+            'INSERT INTO clients (id, name, phone_number) VALUES ($1, $2, $3)',
+            [id, name, phoneNumber]
+        );
+    }
+
+    // insert new branch
+    async insertNewBranch(city: string) {
+        await this.client.query(
+            'INSERT INTO branches (city) VALUES ($1)',
+            [city]
+        );
+    }
+
+    // insert new record
+    async insertNewRecord(branch_code: number, service_code: number, client_id: number) {
+        await this.client.query(
+            'INSERT INTO records (branch_code, client_id, service_code) VALUES ($1,$2,$3)',
+            [branch_code, client_id, service_code]
+        );
+    }
+
+    // return all records from DB
+    async getAllRecords() {
+        const result = await this.client.query(
+            'SELECT number, branches.city, clients.name, clients.phone_number,status, services.description\
+             FROM records \
+             INNER JOIN branches ON \
+             branches.code = records.branch_code \
+             INNER JOIN clients ON \
+             clients.id = records.client_id \
+             INNER JOIN services ON \
+             services.code = records.service_code \
+             ORDER BY number\
+             '
+        )
+        // .catch(err => console.log(err))
+        // console.log(result.rows)
+        return result.rows
+    }
+
+    // return all services types from DB
+    async getAllServices() {
+        const result = await this.client.query(
+            'SELECT * FROM services'
+        );
+        return result.rows
+    }
+
+    // return branches list from DB
+    async getAllBranches() {
+        const result = await this.client.query(
+            'SELECT * FROM branches'
+        );
+        return result.rows
+    }
+
+    // get client info by id
+    async getClientById(id: number) {
+        const result = await this.client.query(`SELECT * FROM clients WHERE id = $1 `, [id]);
+        return result;
+    }
+
+    // get all status options
+    async getStatusOptions() {
+        const result = await this.client.query('select enum_range(null::status)');
+        return result;
+    }
+
+    // update recorde status
+    async updateRecordStatus(newStatus: string, recordNumber: number) {
+        try {
+            const result = await this.client.query(`update records set status = $1 where number = $2;`,
+                [newStatus, recordNumber]);
+            console.log(result)
+            return true
+        }
+        catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,109 @@
 import path from 'path';
-import glob from 'glob'
 import express, { Express } from 'express';
-
-glob('../**/index.html', (err, files) => {
-    if (err) console.log(err);
-    // console.log(files);
-})
+import cors from 'cors';
+import { json } from 'body-parser';
+import DB from './db';
 
 
 const app: Express = express();
-const port = process.env.PORT || 4002;
+const db = new DB();
+const port = process.env.PORT || 4000;
+
+// Create data base connection
+db.connect()
+    .then(async () => {
+        console.log("connected to DB");
+    })
+    .catch((err) => console.log("DB connection failed", err))
+
+
+
+// app.use(cors());
+app.use(json());
 const root: string = path.join(process.cwd(), '../');
-console.log(root)
-console.log('************')
+
+
+// Middleware to log the requested URL and send a server error if database is not connected
+app.use("*", (req, res, next) => {
+    console.log(`${req.method}: ${req.baseUrl}`);
+    next();
+});
+
 
 // serve static files
 app.use(express.static(path.join(root, '/client/build')));
-console.log(root)
+
+
+
+// endpoint to get all records
+app.get('/api/records', async (req, res) => {
+    console.log("request for records");
+    const allRecords = await db.getAllRecords();
+    res.send(allRecords)
+});
+
+// endpoint to get list of all services
+app.get('/api/services', async (req, res) => {
+    const allServices = await db.getAllServices();
+    res.send(allServices)
+});
+
+// endpoint to get list of all branches
+app.get('/api/branches', async (req, res) => {
+    const branchesList = await db.getAllBranches();
+    res.send(branchesList)
+});
+
+// endpoint to get all status options
+app.get('/api/status-options', async (req, res) => {
+    const list = await db.getStatusOptions();
+    let obj = list.rows[0].enum_range;
+    obj = obj.replace('{', '').replace('}', '').replace('"', '').replace('"', '')
+    obj = obj.split(',');
+    res.json(obj);
+})
+
+// endpoint to update record status
+app.put('/api/update-record-status/:recordNumber/:newstatus', async (req, res) => {
+    const recordNumber = Number(req.params.recordNumber)
+    const newStatus = req.params.newstatus
+    console.log(recordNumber, newStatus);
+    const result = await db.updateRecordStatus(newStatus, recordNumber);
+    console.log(result);
+
+})
+
+// endpoint to insert new record
+app.post('/api/insertNewRecord', async (req, res) => {
+    console.log(req.body)
+    // let service, branch, client_id, client_nmae, phone_number;
+    const { service, branch, client_id, client_name, phone_number } = req.body
+    // console.log(req.body)
+    try {
+        // check if client exist in the system
+        const client = await db.getClientById(client_id);
+        console.log(client.rowCount);
+
+        // if new client, add him to the system
+        if (client.rowCount === 0) // new client
+            await db.insertNewClient(client_id, client_name, phone_number)
+
+        // finaly add new record
+        await db.insertNewRecord(branch, service, client_id,);
+        res.json({ msg: 'new record inserted' });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// 
 app.get('*', function (req, res) {
     // console.log(path.join(root, 'deploy/client/index.html'))
     console.log("root", root)
     console.log(req.originalUrl)
     res.sendFile(path.join(root, 'client/build/index.html'));
 });
+
 
 
 app.listen(port, () => {
